bd36662594510c39ae4be9d9737af0c4384a5948
zerofruit <14wnrkim@gmail.com>
2020-10-19 20:54:14 +0900

    init project

---
+++ src/main/java/com/example/cicdproto/demo/DemoApplication.java
@@ -0,0 +1,27 @@
+package com.example.cicdproto.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    @Value("${foo}")
+    private String foo;
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @RestController
+    public class MyController {
+        @GetMapping("/hello")
+        public String helloWorld() {
+            return foo;
+        }
+    }
+
+}
