781d88a4b891747fe31892992b49daa344070fa4
zerofruit <14wnrkim@gmail.com>
2020-10-20 01:56:55 +0900

    edit run-cd.sh script

--- scripts/run-cd.sh
+++ scripts/run-cd.sh
@@ -23,7 +23,8 @@ for FILE in $DIFF_FILES; do
     if grep -q "application.yaml" <<< "$ROOT/$FILE"; then
 
         # ConfigMap 생성 로직
-        DEST="$(dirname $FILE)/config.yaml"
+        DEST="$ROOT/$(dirname $FILE)/config.yaml"
+        echo "Dest: $DEST"
 
         echo "apiVersion: v1
 kind: ConfigMap
@@ -33,11 +34,11 @@ data:
   application.yaml: |" > $DEST
 
         # Print application.yaml file. Each line start with 4 whitespaces
-        cat $FILE | awk '{print "    " $0}' >> $DEST
+        cat $ROOT/$FILE | awk '{print "    " $0}' >> $DEST
 
 
         # 생성한 ConfigMap을 클론한 디렉토리로 복사
-        cp "$ROOT/$DEST" config.yaml
+        cp $DEST config.yaml
 
         git add .
 
@@ -48,44 +49,37 @@ done
 
 cd $ROOT
 
-#
-#
-######
-## Deployment 업데이트
-######
-#
-#function update_config {
-#
-#    cd cicd-demo-manifest/manifest/demo-app
-#
-#    kubectl patch --local -f deployment.yaml -p "{\
-#        \"spec\":{\
-#            \"template\":{\
-#                \"spec\":{\
-#                    \"containers\":[{\
-#                        \"name\":\"demo-app\",\
-#                        \"image\":\"$IMAGE\"\
-#                    }]\
-#                }\
-#            }\
-#        }\
-#    }" -o yaml > deployment-updated.yaml;
-#    rm deployment.yaml;
-#    mv deployment-updated.yaml demo-app.yaml
-#
-#    git add .
-#
-#    # 생성한 Deployment로 패치 후 변경사항 커밋
-#    git commit -m "Update demo-app to $IMAGE"
-#    git push
-#
-#}
-#
-#
-#IMAGE="zerofruit/cicd-demo-app:$COMMIT"
-#
-#docker build -t $IMAGE .
-#docker push $IMAGE
-#
-## update config
-#update_config
+
+
+#####
+# Deployment 업데이트
+#####
+IMAGE="zerofruit/cicd-demo-app:$COMMIT"
+
+# Docker 빌드
+docker build -t $IMAGE .
+docker push $IMAGE
+
+# deployment.yaml의 이미지 태그 바꿔치기
+cd cicd-demo-manifest/manifest/demo-app
+
+kubectl patch --local -f deployment.yaml -p "{\
+    \"spec\":{\
+        \"template\":{\
+            \"spec\":{\
+                \"containers\":[{\
+                    \"name\":\"demo-app\",\
+                    \"image\":\"$IMAGE\"\
+                }]\
+            }\
+        }\
+    }\
+}" -o yaml > deployment-updated.yaml;
+rm deployment.yaml;
+mv deployment-updated.yaml deployment.yaml
+
+git add .
+
+# 생성한 Deployment로 패치 후 변경사항 커밋
+git commit -m "Update demo-app to $IMAGE"
+# git push
