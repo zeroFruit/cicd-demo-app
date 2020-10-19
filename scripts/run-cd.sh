e2c7a88541d65130554efec07023e65d60a370a2
zerofruit <14wnrkim@gmail.com>
2020-10-20 01:45:06 +0900

    update application yaml

--- scripts/run-cd.sh
+++ scripts/run-cd.sh
@@ -10,84 +10,82 @@ ROOT=$PWD
 # ConfigMap 업데이트
 #####
 
-function update_manifest {
+DIFF_FILES=$(git diff --name-only HEAD HEAD~1)
 
-    git clone $REPO_URL
-    cd cicd-demo-manifest/manifest/demo-app
+echo $DIFF_FILES
+git clone $REPO_URL
+cd cicd-demo-manifest/manifest/demo-app
 
-    cp "$ROOT/$DEST" config.yaml
+# list filenames contained in `git diff`
+for FILE in $DIFF_FILES; do
 
-    git add .
+    # if filename contains application.yaml execute
+    if grep -q "application.yaml" <<< "$ROOT/$FILE"; then
 
-    # 생성한 ConfigMap로 패치 후 변경사항 커밋
-    git commit -m "Update demo-app manifest $FILE"
+        # ConfigMap 생성 로직
+        DEST="$(dirname $FILE)/config.yaml"
 
-}
-
-function make_configmap {
-    DEST="$(dirname $FILE)/config.yaml"
-
-    echo "apiVersion: v1
+        echo "apiVersion: v1
 kind: ConfigMap
 metadata:
   name: game-demo
 data:
   application.yaml: |" > $DEST
 
-    # Print application.yaml file. Each line start with 4 whitespaces
-    cat $FILE | awk '{print "    " $0}' >> $DEST
-}
+        # Print application.yaml file. Each line start with 4 whitespaces
+        cat $FILE | awk '{print "    " $0}' >> $DEST
 
-# list filenames contained in `git diff`
-for FILE in $(git diff --name-only HEAD HEAD~1); do
 
-    # if filename contains application.yaml execute
-    if grep -q "application.yaml" <<< $FILE; then
-        make_configmap
-        update_manifest
+        # 생성한 ConfigMap을 클론한 디렉토리로 복사
+        cp "$ROOT/$DEST" config.yaml
+
+        git add .
+
+        # 생성한 ConfigMap로 패치 후 변경사항 커밋
+        git commit -m "Update demo-app manifest $FILE"
     fi
 done
 
 cd $ROOT
 
-
-
-#####
-# Deployment 업데이트
-#####
-
-function update_config {
-
-    cd cicd-demo-manifest/manifest/demo-app
-
-    kubectl patch --local -f deployment.yaml -p "{\
-        \"spec\":{\
-            \"template\":{\
-                \"spec\":{\
-                    \"containers\":[{\
-                        \"name\":\"demo-app\",\
-                        \"image\":\"$IMAGE\"\
-                    }]\
-                }\
-            }\
-        }\
-    }" -o yaml > deployment-updated.yaml;
-    rm deployment.yaml;
-    mv deployment-updated.yaml demo-app.yaml
-
-    git add .
-
-    # 생성한 Deployment로 패치 후 변경사항 커밋
-    git commit -m "Update demo-app to $IMAGE"
-    git push
-
-}
-
-
-IMAGE="zerofruit/cicd-demo-app:$COMMIT"
-
-docker build -t $IMAGE .
-docker push $IMAGE
-
-# update config
-update_config
+#
+#
+######
+## Deployment 업데이트
+######
+#
+#function update_config {
+#
+#    cd cicd-demo-manifest/manifest/demo-app
+#
+#    kubectl patch --local -f deployment.yaml -p "{\
+#        \"spec\":{\
+#            \"template\":{\
+#                \"spec\":{\
+#                    \"containers\":[{\
+#                        \"name\":\"demo-app\",\
+#                        \"image\":\"$IMAGE\"\
+#                    }]\
+#                }\
+#            }\
+#        }\
+#    }" -o yaml > deployment-updated.yaml;
+#    rm deployment.yaml;
+#    mv deployment-updated.yaml demo-app.yaml
+#
+#    git add .
+#
+#    # 생성한 Deployment로 패치 후 변경사항 커밋
+#    git commit -m "Update demo-app to $IMAGE"
+#    git push
+#
+#}
+#
+#
+#IMAGE="zerofruit/cicd-demo-app:$COMMIT"
+#
+#docker build -t $IMAGE .
+#docker push $IMAGE
+#
+## update config
+#update_config
